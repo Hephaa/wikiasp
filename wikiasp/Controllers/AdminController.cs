114182bfbc6548172737cef92ea5a3c2cc30556e
Barış Atış <dhephaistos@gmail.com>
2017-04-14 20:07:14 +0300

    Admin checks for admin panel

---
+++ wikiasp/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using wikiasp.Models;
+
+namespace wikiasp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            if(!Request.IsAuthenticated)
+                return Redirect("/");
+
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var user = manager.FindById(User.Identity.GetUserId());
+            if (user.UserType != 2)
+            {
+                return Redirect("/");
+            }
+
+            return View();
+        }
+    }
+}
